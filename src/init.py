bdad1962235e26cc31a9aff88ccc1f80c0c646f4
Cassondra Dale <cassondradale@noreply.github.com>
2018-05-20 22:19:08 -0700

    added creation of dog name

--- src/init.py
+++ src/init.py
@@ -7,13 +7,15 @@ def main():
 
     valid_breed = False
     while not valid_breed:
-        choice = input("Please select your pupper: ")
-        if choice in puppers:
+        breed_choice = input("Please select your pupper: ")
+        if breed_choice in puppers:
             print("Congrats! You have a new dog!")
             valid_breed = True
         else:
             print("Please select a valid breed.")
 
+    pupper_name = input("Create a name for your new pupper: ")
 
+    print("Your pupper's name is {0} and the breed is {1}".format(pupper_name, breed_choice))
 if __name__ == "__main__":
     main()

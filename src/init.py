0b6a42f20775c920ec05f9862b5a4a93b9ec70d2
Cassondra Dale <cassondradale@noreply.github.com>
2018-05-20 22:09:39 -0700

    added selection of breed type

--- src/init.py
+++ src/init.py
@@ -1,10 +1,19 @@
 
 
-
 def main():
-    print("Hi friend")
+    puppers = ["Dalmation", "Corgi", "Beagle", "Golden Retriever"]
+    puppers.append("Wheaten Terrier")
+    print(puppers)
+
+    valid_breed = False
+    while not valid_breed:
+        choice = input("Please select your pupper: ")
+        if choice in puppers:
+            print("Congrats! You have a new dog!")
+            valid_breed = True
+        else:
+            print("Please select a valid breed.")
 
 
 if __name__ == "__main__":
     main()
-    
